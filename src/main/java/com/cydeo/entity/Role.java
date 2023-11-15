1d9fcb9d3b52d88a1b354c4066762b65ff896144
Archie-f <akif.mk66@gmail.com>
2023-11-15 16:32:42 +0300

    Part-1: Role and User entities modified

--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 @Table(name = "roles")
 public class Role extends BaseEntity{
 
