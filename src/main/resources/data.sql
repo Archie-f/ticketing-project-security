80f3f9730708c0a0a83ea7759751c51ba1f8d366
Archie-f <akif.mk66@gmail.com>
2023-11-16 21:12:52 +0300

    Part-2: Started. listAllUsers implemented.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 INSERT INTO roles(description) VALUES('Admin');
 INSERT INTO roles(description) VALUES('Manager');
 INSERT INTO roles(description) VALUES('Employee');
+
+INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Phoebe','Buffay',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Admin'));
+INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Monica','Geller',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'));
+INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Ross','Geller',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'));
+INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Rachel','Green',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'));
+INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Chandler','Bing',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'));
+INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Joey','Tribiani',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'));
