4c46fa8cf583bea7f8e76f892467d432e8d7587d
Archie-f <akif.mk66@gmail.com>
2023-11-17 03:17:50 +0300

    Part-2: Continue. @Where annotation corrected

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ INSERT INTO roles(description) VALUES('Admin');
 INSERT INTO roles(description) VALUES('Manager');
 INSERT INTO roles(description) VALUES('Employee');
 
-INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Phoebe','Buffay',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Admin'));
-INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Monica','Geller',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'));
-INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Ross','Geller',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'));
-INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Rachel','Green',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'));
-INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Chandler','Bing',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'));
-INSERT INTO users(first_name,last_name,enabled,gender,role_id) VALUES('Joey','Tribiani',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'));
+INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Phoebe','Buffay',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Admin'),false);
+INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Monica','Geller',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'),false);
+INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Ross','Geller',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'),false);
+INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Rachel','Green',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'),false);
+INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Chandler','Bing',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'),false);
+INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Joey','Tribiani',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'),false);
