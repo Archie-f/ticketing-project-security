80962b9e857fbaa018615b08bf72efc1b96eb0d3
Archie-f <akif.mk66@gmail.com>
2023-11-21 21:14:33 +0300

    Part-3: Continue. @PreUpdate, nullable = false, updatable = false

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO roles(description) VALUES('Admin');
-INSERT INTO roles(description) VALUES('Manager');
-INSERT INTO roles(description) VALUES('Employee');
+insert into roles(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, description)
+VALUES ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Admin'),
+       ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Manager'),
+       ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Employee');
 
-INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Phoebe','Buffay',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Admin'),false);
-INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Monica','Geller',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'),false);
-INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Ross','Geller',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Manager'),false);
-INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Rachel','Green',true,'FEMALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'),false);
-INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Chandler','Bing',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'),false);
-INSERT INTO users(first_name,last_name,enabled,gender,role_id,is_deleted) VALUES('Joey','Tribiani',true,'MALE',(SELECT r.id FROM roles r WHERE r.description = 'Employee'),false);
+
+insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
+                  first_name, gender, last_name, user_name, role_id)
+values ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, true, 'admin', 'MALE', 'admin', 'admin@admin.com',
+        1);
